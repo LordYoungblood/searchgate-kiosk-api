f34e29439259000aa4708804e436ee8cdc7ef125
LordYoungblood <ParissYoungblood@gmail.com>
2023-08-25 09:43:54 -0400

    updated the client and the ran npm update

--- db/client.js
+++ db/client.js
@@ -1,15 +1,21 @@
 const { Client } = require("pg");
+
+if (
+  !process.env.PGHOST ||
+  !process.env.PGUSER ||
+  !process.env.PGPORT ||
+  !process.env.PGPASSWORD ||
+  !process.env.PGDATABASE
+) {
+  throw new Error("Database configuration missing in environment variables");
+}
+
 const client = new Client({
-  host: process.env.PGHOST || "vehicle-gate-kiosk.ca4lcabxs5eo.us-gov-west-1.rds.amazonaws.com",
-  user: process.env.PGUSER || 'postgres',
-  port: process.env.PGPORT || 5432,
-  password: process.env.PGPASSWORD || 'password',
-  database: process.env.PGDATABASE || 'gate-kiosk'
-  // host: process.env.DB_HOST,
-  // user: process.env.DB_USER || "postgres",
-  // port: process.env.DB_PORT || 5432,
-  // password: process.env.DB_PASSWORD || "password",
-  // database: process.env.DB_DATABASE || "gatekiosk",
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  port: process.env.PGPORT,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
 });
 
 module.exports = client;
